2fc177a284a9dccf6f228b7d7927d565150cd059
aliebaftijari <51214233+aliebaftijari@users.noreply.github.com>
2019-06-02 14:59:00 +0200

    Add files via upload

---
+++ FAI-lab2/Views/LlojiHapsires/Delete.aspx.cs
@@ -0,0 +1,65 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FAI_lab2.Views.LlojiHapsires
+{
+    public partial class Delete : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblError.Visible = true;
+        }
+
+        protected void btnOK_Click(object sender, EventArgs e)
+        {
+
+            int id = Convert.ToInt32(Session["DeletedID"].ToString());
+            string page = Convert.ToString(Session["PreviousPageName"]);
+
+            try
+            {
+                DeleteForm(id, page);
+
+            }
+            catch (SqlException ex)
+            {
+                if (ex.Errors.Count > 0)
+                {
+                    if (ex.Errors[0].Number == 547)
+                    {
+                        lblError.Text = "Ky entitet nuk mund të fshihet sepse është i ndërlidhur me entitete tjera!";
+                        lblError.Visible = true;
+                        return;
+
+                    }
+                }
+
+            }
+            Response.Redirect(page);
+        }
+
+        private void DeleteForm(int ID, string page)
+        {
+            if (page == "/Views/LlojiHapsires/Index.aspx")
+            {
+                BLL.LlojiHapsires obj = new BLL.LlojiHapsires();
+                obj.LlojiID = ID;
+                LlojiHapsiresMapper objm = new LlojiHapsiresMapper(obj);
+                objm.Delete();
+            }
+        }
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+
+            string page = Convert.ToString(Session["PreviousPageName"]);
+            Response.Redirect(page);
+
+        }
+    }
+}
---
+++ FAI-lab2/Views/LlojiHapsires/Index.aspx.cs
@@ -0,0 +1,66 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DAL;
+using BLL;
+
+namespace FAI_lab2.Views.LlojiHapsires
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        public int SelectedID { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            placeShenimet();
+        }
+
+
+        protected void ListGridView_DataBound(object sender, EventArgs e)
+        {
+            ListGridView.HeaderRow.TableSection = TableRowSection.TableHeader;
+            ListGridView.HeaderStyle.CssClass = "flip-content";
+        }
+        protected void ListGridView_RowDataBound(object sender, System.Web.UI.WebControls.GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.Header)
+            {
+                e.Row.TableSection = TableRowSection.TableHeader;
+                ListGridView.HeaderStyle.CssClass = "flip-content";
+            }
+
+        }
+
+        private void placeShenimet()
+        {
+            List<BLL.LlojiHapsires> lb = new List<BLL.LlojiHapsires>();
+            LloiHapsiraveMappers bm = new LloiHapsiraveMappers(lb);
+            bm.SelectAllLlojiHapsirave();
+            ListGridView.DataSource = lb;
+            ListGridView.DataBind();
+        }
+        protected void ListGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            ListGridView.PageIndex = e.NewPageIndex;
+            placeShenimet();
+        }
+
+        protected void ListGridView_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "EditCommandName")
+            {
+                SelectedID = Convert.ToInt32(e.CommandArgument.ToString());
+                Server.Transfer("Edit.aspx");
+            }
+            else if (e.CommandName == "DeleteCommandName")
+            {
+                Session["DeletedID"] = Convert.ToInt32(e.CommandArgument.ToString());
+                Session["PreviousPageName"] = "/Views/LlojiHapsira/" + System.IO.Path.GetFileName(Page.Request.FilePath);
+                Server.Transfer("Delete.aspx");
+            }
+        }
+    }
+}
